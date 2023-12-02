678e0c78602da8585f72a0dabaed102b2f25f622
luis.chula <luis.chula@lbc-global.com>
2023-12-02 17:12:25 +0000

    added properties page and added reservation modal

---
+++ src/app/components/reservation-modal/reservation-modal.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-reservation-modal',
+  templateUrl: './reservation-modal.component.html',
+  styleUrls: ['./reservation-modal.component.css']
+})
+export class ReservationModalComponent {
+
+}
