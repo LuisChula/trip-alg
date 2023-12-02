292597a0978d78f41101b5ab725b9dd58b917d39
luis.chula <luis.chula@lbc-global.com>
2023-12-02 17:15:29 +0000

    first deploy

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,12 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { HomeComponent } from './pages/home/home.component';
+import { PropertiesComponent } from './pages/properties/properties.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path: '', component: HomeComponent},
+  {path: 'properties', component: PropertiesComponent}
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,22 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HomeComponent } from './pages/home/home.component';
+import { HeaderComponent } from './components/header/header.component';
+import { CarouselComponent } from './components/carousel/carousel.component';
+import { FooterComponent } from './components/footer/footer.component';
+import { PropertiesComponent } from './pages/properties/properties.component';
+import { ReservationModalComponent } from './components/reservation-modal/reservation-modal.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HomeComponent,
+    HeaderComponent,
+    CarouselComponent,
+    FooterComponent,
+    PropertiesComponent,
+    ReservationModalComponent,
   ],
   imports: [
     BrowserModule,
