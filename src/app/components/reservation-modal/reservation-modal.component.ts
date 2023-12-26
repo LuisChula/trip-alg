8b7ef6ed0e3576acbf5cb07327e28d4b81c8eca5
luis.chula <luis.chula@lbc-global.com>
2023-12-26 15:38:56 +0000

    added properties imgs; fixed img carousel

--- src/app/components/reservation-modal/reservation-modal.component.ts
+++ src/app/components/reservation-modal/reservation-modal.component.ts
@@ -1,18 +1,35 @@
-import { Component, Input, OnChanges, SimpleChanges } from '@angular/core';
+import { Component, Input, OnChanges, OnInit, SimpleChanges } from '@angular/core';
 
 @Component({
   selector: 'app-reservation-modal',
   templateUrl: './reservation-modal.component.html',
   styleUrls: ['./reservation-modal.component.css']
 })
-export class ReservationModalComponent {
+export class ReservationModalComponent implements OnInit{
 
   @Input() index!: number;
   @Input() title: string = "Sem titulo";
   @Input() longDescription!: string;
   @Input() commodities!: any[];
+  @Input() allImgs!: any[];
+
+  imgs!: any[];
 
   selectedImg: number = 0;
+
+  start: number = 0;
+  end: number = 2;
+
+  ngOnInit(): void {
+    this.updateImgs();
+  }
+
+  updateImgs() {
+    this.imgs = [];
+    for(let i = this.start; i <= this.end; i++) {
+      this.imgs.push(this.allImgs[i]);
+    }
+  }
 
   setSelectedImg(selected: number) {
     this.selectedImg = selected;
@@ -22,6 +39,13 @@ export class ReservationModalComponent {
     this.selectedImg++;
     if(this.selectedImg > 2) {
       this.selectedImg = 2;
+      this.end++;
+      this.start++;
+      if (this.end >= this.allImgs.length) {
+        this.end--;
+        this.start--;
+      }
+      this.updateImgs();
     }
   }
 
@@ -29,6 +53,13 @@ export class ReservationModalComponent {
     this.selectedImg--;
     if(this.selectedImg < 0) {
       this.selectedImg = 0;
+      this.end--;
+      this.start--;
+      if (this.start < 0) {
+        this.end++;
+        this.start++;
+      }
+      this.updateImgs();
     }
   }
 
